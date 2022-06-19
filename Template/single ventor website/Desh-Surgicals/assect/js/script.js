e28e0b2518d3e7e09bb60b6b90b279d8f7250f9b
tonmoy <shahariya007@gmail.com>
2022-06-20 01:36:48 +0600

    today's work done!

---
+++ Template/single ventor website/Desh-Surgicals/assect/js/script.js
@@ -0,0 +1,11 @@
+
+//menu opne
+$(document).ready(function(){
+    $('.menu-btn').click(function(){
+        $('.side-menu').show();
+    });
+
+    $('.menu-close').click(function(){
+        $('.side-menu').hide();
+    });
+});
